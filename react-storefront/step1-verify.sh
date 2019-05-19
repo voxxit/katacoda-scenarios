2f5ef6e8cefca37d4f0d646e5cf6ebcf59cad0cd
Joshua Delsman <voxxit@users.noreply.github.com>
2019-05-19 12:44:28 -0700

    disable verify for now

--- react-storefront/step1-verify.sh
+++ react-storefront/step1-verify.sh
@@ -1,4 +1,4 @@
-[ -f /etc/apt/sources.list.d/nodesource.list ] \
-[ "$(command -v node)" = "/usr/bin/node" ] \
-[ "$(command -v npm)" = "/usr/bin/npm" ] \
-  && echo "done"
+# [ -f /etc/apt/sources.list.d/nodesource.list ] && \
+# [ "$(command -v node)" = "/usr/bin/node" ] && \
+# [ "$(command -v npm)" = "/usr/bin/npm" ] && \
+# echo \"done\"
