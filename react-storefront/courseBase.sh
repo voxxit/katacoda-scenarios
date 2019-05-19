978f3ccc8c2c21041f8d3ce5bb6f79c17a7db7e5
Joshua Delsman <voxxit@users.noreply.github.com>
2019-05-19 14:41:40 -0700

    try this

--- react-storefront/courseBase.sh
+++ react-storefront/courseBase.sh
@@ -1,5 +1,8 @@
 # Clear old files from the working directory
-rm -rf myapp/
+rm -rf /root/*
+
+# Make sure the permissions are right
+chown -R 1000:1000 /root
 
 # Reset the Docker environment
 docker rm -fv $(docker ps -a -q)
